25668b86df5287336f082371e16ecb3c39ec5f24
dilanrussy-code <dilanrussy@gmail.com>
2026-09-24 21:47:06 -0500

    Update and rename README.md to README.py

--- README.py
+++ README.py
@@ -1,4 +1,9 @@
 <img width="435" height="607" alt="Captura de pantalla 2026-09-24 213726" src="https://github.com/user-attachments/assets/208333f6-090e-4820-8784-5b095071b1aa" />
+
+
+
+
+
 class Componente:
     def __init__(self, codigo, nombre, cantidad, estado):
         self.codigo = codigo
